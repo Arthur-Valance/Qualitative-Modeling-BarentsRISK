482975510a96997b5493e891b66c0d7158a1678d
Arthur Valance <arthur.valance@agrocampus-ouest.fr>
2021-06-04 09:52:01 +0200

    Clear plot_increasing_pressures.R function / Add cumulative pressures analyses in Experiments.R / Add plot_crossed_pressures.R function

--- Experiments.R
+++ Experiments.R
@@ -2,7 +2,7 @@
 
 # Set the working directory -----------------------------------------------
 
-rm(list=ls())
+#rm(list=ls())
 getwd()
 #setwd(dir="")
 
@@ -30,6 +30,7 @@ source("./InferenceQM.R")
 load("./ws.Rdata")
 load("./As.Rdata")
 load("./edges.Rdata")
+
 
 
 # Scenarios ---------------------------------------------------------------
@@ -74,18 +75,88 @@ plot_QM_VS_FCM(results)
 
 #### Cumulative impacts
 
+
+## Increasing Fishing
+
 # Scenarios ---------------------------------------------------------------
 
-scenarios_increasing_pressures <- as.matrix(read_xlsx("./METADATA.xlsx",sheet="Scenarios_Increasing_Pressures",range = "B2:L18"))
-colnames(scenarios_increasing_pressures) <- NULL
-Scenarios_increasing_pressures <- split(scenarios_increasing_pressures, rep(1:ncol(scenarios_increasing_pressures), each = nrow(scenarios_increasing_pressures)))
+
+
+scenarios_increasing_fishing <- as.matrix(read_xlsx("./METADATA.xlsx",sheet="Scenarios_Increasing_Fishing",range = "B2:L18"))
+colnames(scenarios_increasing_fishing) <- NULL
+Scenarios_increasing_fishing <- split(scenarios_increasing_fishing, rep(1:ncol(scenarios_increasing_fishing), each = nrow(scenarios_increasing_fishing)))
+
+
+
+# Plot --------------------------------------------------------------------
+
+
+inverse_sigmoid <- function(x,lambda=2,h=0){
+  return(((-1)/lambda)*log((1-x)/x)-h)
+}
+
+inv_sig_hidden_pattern <- inverse_sigmoid(inferenceFCM(rep(0,length(interaction_matrix)),lambda=2,h=0,interaction_matrix))
+
+source("./plot_increasing_pressures.R")
+FCM_increasing_fishing <- purrr::map(Scenarios_increasing_fishing,function(x) {
+  inverse_sigmoid(inferenceFCM(x,lambda=2,h=0,interaction_matrix))-inv_sig_hidden_pattern}
+) %>% unlist
+pressures <- c(0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1)
+plot_increasing_pressures(FCM_increasing_fishing,Scenarios_increasing_fishing)
+
+# split_FCM_increasing_fishing  <- split(FCM_increasing_fishing ,rep(1:length(interaction_matrix), length(Scenarios_increasing_fishing)))
+# for (i in 1:length(interaction_matrix)){
+#   split_FCM_increasing_fishing [[i]] <- unname(split_FCM_increasing_fishing [[i]])
+# }
+# split_FCM_increasing_fishing 
+  
+
+
+## Increasing Petroleum Activity
+
+
+# Scenarios ---------------------------------------------------------------
+
+scenarios_increasing_petrolact <- as.matrix(read_xlsx("./METADATA.xlsx",sheet="Scenarios_Increasing_PetrolAct",range = "B2:L18"))
+colnames(scenarios_increasing_petrolact) <- NULL
+Scenarios_increasing_petrolact <- split(scenarios_increasing_petrolact, rep(1:ncol(scenarios_increasing_petrolact), each = nrow(scenarios_increasing_petrolact)))
 
 
 
 # Plot --------------------------------------------------------------------
 
 source("./plot_increasing_pressures.R")
-FCM_inc_press <- purrr::map(Scenarios_increasing_pressures,function(x) {
-  inferenceFCM(x,lambda=2,h=0,interaction_matrix)-hidden_pattern}
+FCM_increasing_petrolact <- purrr::map(Scenarios_increasing_petrolact,function(x) {
+  inverse_sigmoid(inferenceFCM(x,lambda=2,h=0,interaction_matrix))-inv_sig_hidden_pattern}
 ) %>% unlist
-plot_increasing_pressures(FCM_inc_press)
+plot_increasing_pressures(FCM_increasing_petrolact,Scenarios_increasing_petrolact)
+
+# split_FCM_increasing_petrolact <- split(FCM_increasing_petrolact,rep(1:length(interaction_matrix), length(Scenarios_increasing_fishing)))
+# for (i in 1:length(interaction_matrix)){
+#   split_FCM_increasing_petrolact[[i]] <- unname(split_FCM_increasing_petrolact[[i]])
+# }
+# split_FCM_increasing_petrolact
+
+
+# Crossed scenarios -------------------------------------------------------
+
+crossed_scenarios <- as.matrix(read_xlsx("./METADATA.xlsx",sheet="Scenarios_Fishing_X_PetrolAct",range = "B2:M18"))
+colnames(crossed_scenarios) <- NULL
+crossed_Scenarios <- split(crossed_scenarios, rep(1:ncol(crossed_scenarios), each = nrow(crossed_scenarios)))
+
+
+
+FCM_crossed_scenarios <- purrr::map(crossed_Scenarios,function(x) {
+  inverse_sigmoid(inferenceFCM(x,lambda=2,h=0,interaction_matrix))-inv_sig_hidden_pattern}
+) %>% unlist
+
+# split_FCM_crossed_scenarios <- split(FCM_crossed_scenarios,rep(1:length(interaction_matrix), length(crossed_Scenarios)))
+# for (i in 1:length(interaction_matrix)){
+#   split_FCM_crossed_scenarios[[i]] <- unname(split_FCM_crossed_scenarios[[i]])
+# }
+# split_FCM_crossed_scenarios
+
+source("./plot_crossed_pressures.R")
+library("gridExtra")
+plot_crossed_pressures(FCM_crossed_scenarios,crossed_Scenarios)
+
---
+++ plot_crossed_pressures.R
@@ -0,0 +1,99 @@
+plot_crossed_pressures <- function(FCM_cros_press,crossed_scenars){
+  
+
+  # Fishing x Petroleum Activity
+  
+  pollution <- subset(data.frame("value" = FCM_cros_press,"vertice"=rep(colnames(interaction_matrix),length(crossed_scenars))),vertice == "Pollution")
+  area_conflict <- subset(data.frame("value" = FCM_cros_press,"vertice"=rep(colnames(interaction_matrix),length(crossed_scenars))),vertice == "Area Conflict")
+  seabirds <- subset(data.frame("value" = FCM_cros_press,"vertice"=rep(colnames(interaction_matrix),length(crossed_scenars))),vertice == "Seabirds")
+  icespecies <- subset(data.frame("value" = FCM_cros_press,"vertice"=rep(colnames(interaction_matrix),length(crossed_scenars))),vertice == "Ice Species")
+  provisioning_service <- subset(data.frame("value" = FCM_cros_press,"vertice"=rep(colnames(interaction_matrix),length(crossed_scenars))),vertice == "Provisioning Service")
+  
+  sum_up <- c(pollution$value[1],
+              pollution$value[2],
+              pollution$value[3],
+              pollution$value[4],
+              pollution$value[5],
+              pollution$value[6],
+              area_conflict$value[1],
+              area_conflict$value[2],
+              area_conflict$value[3],
+              area_conflict$value[4],
+              area_conflict$value[5],
+              area_conflict$value[6],
+              seabirds$value[1],
+              seabirds$value[2],
+              seabirds$value[3],
+              seabirds$value[4],
+              seabirds$value[5],
+              seabirds$value[6],
+              icespecies$value[1],
+              icespecies$value[2],
+              icespecies$value[3],
+              icespecies$value[4],
+              icespecies$value[5],
+              icespecies$value[6],
+              provisioning_service$value[1],
+              provisioning_service$value[2],
+              provisioning_service$value[3],
+              provisioning_service$value[4],
+              provisioning_service$value[5],
+              provisioning_service$value[6])
+  
+  crossed_pressures_Fishing_X_PetrolAct <- data.frame("Impacted vertice"= c(rep(c("Pollution","Pollution","Pollution cumulative"),2),
+                                                                                rep(c("Area Conflict","Area Conflict","Area Conflict cumulative"),2),
+                                                                                rep(c("Seabirds","Seabirds","Seabirds cumulative"),2),
+                                                                                rep(c("Ice Species","Ice Species","Ice Species cumulative"),2),
+                                                                                rep(c("Provisioning Service","Provisioning Service","Provisioning Service cumulative"),2)),
+                                                      "Perturbation"=sum_up,
+                                                      "Scenario"=rep(c("Fg1/PA0","Fg0/PA1","Fg1/PA1","Fg-1/PA0","Fg0/PA-1","Fg-1/PA-1"),5))
+  plot_results <- ggplot(crossed_pressures_Fishing_X_PetrolAct,aes(x=Impacted.vertice,y=Perturbation)) +
+                  geom_col(aes(fill=Scenario)) + 
+                  theme(axis.text.x=element_text(angle=45))
+  
+  
+  # same for fish X Marine Mammals
+  
+  MarineMammals <- subset(data.frame("value" = FCM_cros_press,"vertice"=rep(colnames(interaction_matrix),length(crossed_scenars))),vertice == "Marine Mammals")
+  Icespecies <- subset(data.frame("value" = FCM_cros_press,"vertice"=rep(colnames(interaction_matrix),length(crossed_scenars))),vertice == "Ice Species")
+  Shellfish <- subset(data.frame("value" = FCM_cros_press,"vertice"=rep(colnames(interaction_matrix),length(crossed_scenars))),vertice == "Shellfish and Other Bottom Living Animals")
+  CulturalService <- subset(data.frame("value" = FCM_cros_press,"vertice"=rep(colnames(interaction_matrix),length(crossed_scenars))),vertice == "Cultural Service")
+  
+  
+  sum_up_2 <- c(MarineMammals$value[7],
+              MarineMammals$value[8],
+              MarineMammals$value[9],
+              MarineMammals$value[10],
+              MarineMammals$value[11],
+              MarineMammals$value[12],
+              Icespecies$value[7],
+              Icespecies$value[8],
+              Icespecies$value[9],
+              Icespecies$value[10],
+              Icespecies$value[11],
+              Icespecies$value[12],
+              Shellfish$value[7],
+              Shellfish$value[8],
+              Shellfish$value[9],
+              Shellfish$value[10],
+              Shellfish$value[11],
+              Shellfish$value[12],
+              CulturalService$value[7],
+              CulturalService$value[8],
+              CulturalService$value[9],
+              CulturalService$value[10],
+              CulturalService$value[11],
+              CulturalService$value[12])
+  
+  crossed_pressures_Fish_X_MarineMammals <- data.frame("Impacted vertice"= c(rep(c("Marine Mammals","Marine Mammals","Marine Mammals cumulative"),2),
+                                                                            rep(c("Ice Species","Ice Species","Ice Species cumulative"),2),
+                                                                            rep(c("Shellfish and Other Bottom Living Animals","Shellfish and Other Bottom Living Animals","Shellfish and Other Bottom Living Animals cumulative"),2),
+                                                                            rep(c("Cultural Service","Cultural Service","Cultural Service cumulative"),2)),
+                                                      "Perturbation"=sum_up_2,
+                                                      "Scenario"=rep(c("F1/MM0","F0/MM1","F1/MM1","F-1/MM0","F0/MM-1","F-1/MM-1"),4))
+  plot_results_2 <- ggplot(crossed_pressures_Fish_X_MarineMammals,aes(x=Impacted.vertice,y=Perturbation)) +
+    geom_col(aes(fill=Scenario)) + 
+    theme(axis.text.x=element_text(angle=45))
+  
+  plot_grid(plot_results, plot_results_2, labels=c("A", "B"), ncol = 1, nrow = 2)
+}
